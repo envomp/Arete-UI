b61d89135de842653ee6033d567a2d2351698978
envomp <envomp@ttu.ee>
2020-02-10 20:44:44 +0200

    table fields

--- src/components/DashViews/CourseTable.vue
+++ src/components/DashViews/CourseTable.vue
@@ -9,6 +9,7 @@
                 text="Course overview"
                 title="Course Table"
         >
+
             <v-text-field
                     append-icon="search"
                     hide-details
@@ -66,13 +67,86 @@
                             >
                                 <v-window-item>
 
-                                    <v-card flat id="fullCourse" >
-                                        <div v-html="fullCourse"></div>
-                                    </v-card>
+                                    <material-card
+                                            :color="color"
+                                            text="Student data regarding the course only"
+                                            title="Students taking the course"
+                                    >
+                                        <v-text-field
+                                                append-icon="search"
+                                                hide-details
+                                                label="Search"
+                                                single-line
+                                                v-model="subSearch"
+                                        ></v-text-field>
 
+                                        <v-data-table
+
+                                                :headers="subHeaders"
+                                                :items="fullCourse.students"
+                                                :rows-per-page-items="rowsAmount"
+                                                :search="subSearch"
+                                                class="elevation-1"
+
+                                        >
+
+                                            <template
+                                                    slot="headerCell"
+                                                    slot-scope="{ header }"
+                                            >
+                <span
+                        v-bind:class="'subheading font-weight-light text-' + color"
+                        v-text="header.text"
+                />
+                                            </template>
+
+                                            <template
+                                                    v-slot:items="props"
+                                            >
+                                                <tr @click="props.expanded = !props.expanded">
+                                                    <td>{{ props.item.id }}</td>
+                                                    <td>{{ props.item.student.uniid }}</td>
+                                                    <td>{{ props.item.latestSubmission }}</td>
+                                                    <td>{{ props.item.totalCommits }}</td>
+                                                    <td>{{ props.item.totalTestsRan }}</td>
+                                                    <td>{{ props.item.totalTestsPassed }}</td>
+                                                    <td>{{ props.item.totalDiagnosticErrors }}</td>
+                                                    <td>{{ props.item.differentSlugs }}</td>
+                                                    <td>{{ props.item.commitsStyleOK }}</td>
+                                                </tr>
+                                            </template>
+
+                                            <v-spacer></v-spacer>
+
+                                            <template v-slot:expand="props">
+
+                                                <v-container>
+                                                    <v-layout row wrap>
+                                                        <v-flex>
+
+                                                            <v-window
+                                                                    class="elevation-1"
+                                                            >
+                                                                <v-window-item>
+
+                                                                    <v-card flat id="fullStudentCourse">
+                                                                        <div v-html="props.item.student"></div>
+                                                                    </v-card>
+
+                                                                </v-window-item>
+                                                            </v-window>
+
+                                                        </v-flex>
+                                                    </v-layout>
+                                                </v-container>
+
+                                            </template>
+
+                                        </v-data-table>
+
+                                    </material-card>
                                 </v-window-item>
                             </v-window>
-
                         </v-flex>
                     </v-layout>
                 </v-container>
@@ -98,6 +172,7 @@
             fullCourse: [],
             rowsAmount: [15, 20, 25, {"text": "$vuetify.dataIterator.rowsPerPageAll", "value": -1}],
             search: '',
+            subSearch: '',
             headers: [
                 {text: 'id', align: 'left', value: 'id'},
                 {text: 'name', value: 'name'},
@@ -108,6 +183,17 @@
                 {text: 'differentStudents', value: 'differentStudents'},
                 {text: 'commitsStyleOK', value: 'commitsStyleOK'},
             ],
+            subHeaders: [
+                {text: 'id', align: 'left', value: 'id'},
+                {text: 'uniid', value: 'uniid'},
+                {text: 'latestSubmission', value: 'latestSubmission'},
+                {text: 'totalCommits', value: 'totalCommits'},
+                {text: 'totalTestsRan', value: 'totalTestsRan'},
+                {text: 'totalTestsPassed', value: 'totalDiagnosticErrors'},
+                {text: 'totalDiagnosticErrors', value: 'totalDiagnosticErrors'},
+                {text: 'differentSlugs', value: 'differentSlugs'},
+                {text: 'commitsStyleOK', value: 'commitsStyleOK'},
+            ]
         }),
 
         computed: {
