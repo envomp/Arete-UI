70c7e44e738d9943de74153a0eb62e34ef8d7215
envomp <envomp@ttu.ee>
2020-03-15 16:38:30 +0200

    change backend url

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ import vuetify from './plugins/vuetify';
 
 Vue.prototype.$http = axios;
 // Sets the default url used by all of this axios instance's requests
-axios.defaults.baseURL = 'http://localhost:8001/admin/';
+axios.defaults.baseURL = 'http://localhost:8001/services/arete/api/admin/';
 axios.defaults.headers.get['Accept'] = 'application/json';
 
 const token = localStorage.getItem('token');
