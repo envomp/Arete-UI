848110d9ef2d98827bda3f6222837e4505391a96
envomp <envomp@ttu.ee>
2020-09-22 14:41:29 +0300

    change view

--- src/components/DashViews/SubmissionTable.vue
+++ src/components/DashViews/SubmissionTable.vue
@@ -1056,7 +1056,26 @@ export default {
 		},
 
 		createFileView(i) {
-			return `<pre>${this.fullSubmission[i]}</pre>>`;
+			return `<pre>${this.syntaxHighlight(JSON.stringify(this.fullSubmission[i], undefined, 4))}</pre>`;
+		},
+
+		syntaxHighlight(json) {
+			json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+			return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+				var cls = 'number';
+				if (/^"/.test(match)) {
+					if (/:$/.test(match)) {
+						cls = 'key';
+					} else {
+						cls = 'string';
+					}
+				} else if (/true|false/.test(match)) {
+					cls = 'boolean';
+				} else if (/null/.test(match)) {
+					cls = 'null';
+				}
+				return '<span class="' + cls + '">' + match + '</span>';
+			});
 		}
 
 	},
@@ -1064,5 +1083,29 @@ export default {
 </script>
 
 <style scoped>
+pre {
+	outline: 1px solid #ccc;
+	padding: 5px;
+	margin: 5px;
+}
 
+.string {
+	color: green;
+}
+
+.number {
+	color: darkorange;
+}
+
+.boolean {
+	color: blue;
+}
+
+.null {
+	color: magenta;
+}
+
+.key {
+	color: red;
+}
 </style>
